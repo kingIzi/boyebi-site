0c4dabd5626885c3c16ab2946ffb18f892bdac7e
izi <ki@cocoders.one>
2023-02-16 14:04:03 +0100

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ReadComponent } from './pages/read/read.component';
 import { StreamingComponent } from './pages/streaming/streaming.component';
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'streaming/:id',
     component: StreamingComponent,
+  },
+  {
+    path: 'read/:id',
+    component: ReadComponent,
   },
 ];
 
